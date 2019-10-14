49f2dbe02bd9ec2da87218bfd7f5b84d9f532808
Kayo Fujii <kayosamu1025@gmail.com>
2019-10-14 01:27:02 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -15,5 +21,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+  
 
 end
